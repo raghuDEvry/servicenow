9d1fedf55025b4c9506120dc710ebdfece5ad07d
raghuDEvry <raghu.diggi@tietoevry.com>
2024-10-01 23:49:39 +0530

    Update Contact.java

--- src/main/java/com/example/servicenow/entity/Contact.java
+++ src/main/java/com/example/servicenow/entity/Contact.java
@@ -13,4 +13,19 @@ public class Contact {
     private String message;
 
     // Getters and setters
+     public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
 }
